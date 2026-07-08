3f704e24bd9834ea83b3ae8972a153c78dffca96
Fahmy Mahmoud <fahmy.14.mahmoud@gmail.com>
2026-07-08 17:38:15 +0300

    Add app color palette.

--- app/src/main/java/com/linkdevelopment/android_tasks/ui/theme/Color.kt
+++ app/src/main/java/com/linkdevelopment/android_tasks/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryGreen = Color(0xFF006644)
+val LightGreenBg = Color(0xFFB0E1C7)
+val LightGreyBg = Color(0xFFD9D3D3)
+val Black = Color(0xFF0C0C0C)
