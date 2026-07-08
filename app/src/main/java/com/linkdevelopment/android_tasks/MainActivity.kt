add8e29347999a47ade2e93fa3e758e4b40e87bd
Fahmy Mahmoud <fahmy.14.mahmoud@gmail.com>
2026-07-09 01:00:30 +0300

    Add task screen with add and delete functionality.

--- app/src/main/java/com/linkdevelopment/android_tasks/MainActivity.kt
+++ app/src/main/java/com/linkdevelopment/android_tasks/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.linkdevelopment.android_tasks.ui.screens.TaskScreen
 import com.linkdevelopment.android_tasks.ui.theme.AndroidTasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidTasksTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TaskScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidTasksTheme {
-        Greeting("Android")
-    }
-}
