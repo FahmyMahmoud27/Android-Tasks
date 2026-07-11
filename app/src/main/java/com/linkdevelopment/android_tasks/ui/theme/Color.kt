9b97a4adff607ba530e85bfdee9b1feb76784259
Fahmy Mahmoud <fahmy.14.mahmoud@gmail.com>
2026-07-11 18:18:26 +0300

    Add custom theme colors, typography, and shapes

--- app/src/main/java/com/linkdevelopment/android_tasks/ui/theme/Color.kt
+++ app/src/main/java/com/linkdevelopment/android_tasks/ui/theme/Color.kt
@@ -6,9 +6,11 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+val White = Color(0xFFFFFFFF)
+
 val Purple40 = Color(0xFF51459E)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkGrey = Color(0xFF858383)
+val Red = Color(0xFFE50B0B)
 
 val PrimaryGreen = Color(0xFF006644)
 val LightGreenBg = Color(0xFFB0E1C7)
